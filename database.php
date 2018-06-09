a21828693cf062d64cbfbdd2c6587f8b9ef9b7db
Aman Jain <amanjain7898@gmail.com>
2018-06-10 01:23:52 +0530

    handle db connection properly

--- database.php
+++ database.php
@@ -1,4 +1,19 @@
 <?php
+// store all these first into variables and then use
+// $host = 'localhost';
+// $user = 'root';
+// $pass = 'use-password-if-any';
+// $db   = 'todo_list';
+
+// make a connection and check if connection failed then throw error , so that user cannot use app further
+
+// try {
+//  $conn = mysqli_connect ($host, $user, $pass, $db);
+// }catch (Exception $e){
+//  print_r($e);
+//  die();
+// }
+
 $con=mysqli_connect("localhost","root");
 mysqli_select_db($con,'todo_list');
 ?>
